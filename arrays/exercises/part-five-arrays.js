53b7a10fa6cb19bfd912a943e258d5e53d2a2a78
Robert Hasinbiller <roberthasinbiller@Roberts-MBP-2.attlocal.net>
2024-06-20 21:19:09 -0500

    This is a test flow

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -6,6 +6,6 @@ let arr = ['B', 'n', 'n', 5];
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
 
 //3) Do split or join change the original string/array?
-
+zdgsd
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
